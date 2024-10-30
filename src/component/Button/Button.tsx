e9cdc849084b936038915cadee986a51312560da
6teendollars <91662992+6teendollars@users.noreply.github.com>
2024-10-30 19:34:19 +0100

    add: package V:0.0.2

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,12 +1,14 @@
-type Props = {
-	text: string;
-}
-
- const Button:React.FC<Props> = ({ text }) => {
-  return (
-	<div>{text}</div>
-  )
-}
-
-export default Button
-
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	variant?: 'primary' | 'secondary';
+  }
+  
+  const Button: React.FC<ButtonProps> = ({ variant = 'primary', ...props }) => {
+	const colorClass = variant === 'primary' ? 'blue' : 'gray';
+	return (
+	  <button style={{ backgroundColor: colorClass }} {...props}>
+		{props.children}
+	  </button>
+	);
+  };
+  
+  export default Button;

faad223b3dd52af0ea068cc1d1f512a2468e0cca
6teendollars <91662992+6teendollars@users.noreply.github.com>
2024-11-03 09:00:56 +0100

    style: button styling

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,14 +1,20 @@
+import "./Button.css"
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	variant?: 'primary' | 'secondary';
   }
   
   const Button: React.FC<ButtonProps> = ({ variant = 'primary', ...props }) => {
-	const colorClass = variant === 'primary' ? 'blue' : 'gray';
-	return (
-	  <button style={{ backgroundColor: colorClass }} {...props}>
+const colorClasses = () => {
+	if (variant == 'primary') return {borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA'};
+	if (variant == 'secondary') return {borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA'}
+return{}
+}
+
+return (
+	<button style={{...colorClasses(), ...props.style }} {...props}>
 		{props.children}
-	  </button>
-	);
-  };
+	</button>
+)
+   };
   
   export default Button;

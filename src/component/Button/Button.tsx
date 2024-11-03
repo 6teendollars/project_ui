fde6b7e78a43f3c2b32ac11896d8dca2543653a3
6teendollars <91662992+6teendollars@users.noreply.github.com>
2024-11-03 21:54:57 +0100

    add style: Button

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,12 +1,13 @@
 import "./Button.css"
+import React from "react";
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	variant?: 'primary' | 'secondary';
   }
   
   const Button: React.FC<ButtonProps> = ({ variant = 'primary', ...props }) => {
 const colorClasses = () => {
-	if (variant == 'primary') return {borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA'};
-	if (variant == 'secondary') return {borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA'}
+	if (variant == 'primary') return {};
+	if (variant == 'secondary') return {}
 return{}
 }
 
